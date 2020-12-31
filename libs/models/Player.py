babe8de85796e84474a52e791924e70e1eecb968
Cactiw <Knyazev1514@gmail.com>
2020-12-31 15:45:37 +0300

    Fixed None location error

--- libs/models/Player.py
+++ libs/models/Player.py
@@ -83,7 +83,7 @@ class Player(Base):
 
     def update_location(self, location: 'Location', session: Session):
         change = PlayerLocationChanges(player=self, location=location, date=get_current_datetime())
-        if self.location.is_space and not location.is_space:
+        if self.location and self.location.is_space and not location.is_space:
             self.possible_ships.clear()
         self.location = location
         session.add(change)
